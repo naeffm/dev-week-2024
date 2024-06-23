746c438b156be1e6c90b53cd7fa2f645c12e356b
Naeff Matheus <naeffm7@hotmail.com>
2024-06-23 13:39:40 -0300

    Implementacao camada domain

---
+++ src/main/java/me/dio/domain/model/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.domain.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.dio.domain.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
